3d2e949e8c18db353bfc88ebc0af46ff9edc682f
Артур Дементьев <mcnaggets69@mail.ru>
2023-11-24 22:50:11 +0700

    авторизация

--- lab4.py
+++ lab4.py
@@ -2,6 +2,25 @@ from flask import Blueprint, render_template, request
 lab4 = Blueprint('lab4', __name__)
 
 
-@lab3.route('/lab4/')
+@lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+
+@lab4.route('/lab4/login', methods = ['GET', 'POST'])
+def login():
+    errors={}
+    if request.method == 'GET':
+        render_template("login.html")
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == 'alex' and password == '123':
+        return render_template('sucess.html')
+    if username == '':
+        errors['username'] = 'Не введён логин'
+    if password == '':
+        errors['password'] = 'Не введён пароль'
+
+    error = 'Неверные логин и/или пароль'
+    return render_template('login.html', error=error, username=username, password=password, errors=errors)
