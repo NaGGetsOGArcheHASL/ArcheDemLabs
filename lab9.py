ffdcf9b6387b7c495f958ecc7ef15eaee1b04c3b
Артур Дементьев <mcnaggets69@mail.ru>
2024-01-02 17:18:34 +0700

    Обработчик ошибки 500

--- lab9.py
+++ lab9.py
@@ -10,3 +10,12 @@ def main():
 @lab9.app_errorhandler(404)
 def not_found(err):
     return render_template('lab9/Error.html'), 404
+
+
+@lab9.route('/lab9/500')
+def server_error():
+    return render_template("Произошла некоторая ошибка")
+
+@lab9.app_errorhandler(500)
+def er500(err):
+   return render_template('lab9/500.html'), 500
