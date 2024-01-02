60a1ba0de80ac393a49f4e9eec1ce84ebf4ee701
Артур Дементьев <mcnaggets69@mail.ru>
2024-01-02 17:26:49 +0700

    Открытка

--- lab9.py
+++ lab9.py
@@ -19,3 +19,25 @@ def server_error():
 @lab9.app_errorhandler(500)
 def er500(err):
    return render_template('lab9/500.html'), 500
+
+
+@lab9.route('/lab9/greeting', methods=['GET', 'POST'])
+def greeting():
+    class GreetingForm(FlaskForm):
+        recipient_name = StringField('Имя получателя', validators=[DataRequired()])
+        recipient_gender = SelectField('Пол получателя', choices=[('male', 'Мужской'), ('female', 'Женский')], validators=[DataRequired()])
+        sender_name = StringField('Ваше имя', validators=[DataRequired()])
+        submit = SubmitField('Отправить')
+
+    form = GreetingForm()
+
+    if form.validate_on_submit():
+        recipient_name = form.recipient_name.data
+        recipient_gender = form.recipient_gender.data
+        sender_name = form.sender_name.data
+        return redirect(url_for('lab9.show_greeting', recipient_name=recipient_name, recipient_gender=recipient_gender, sender_name=sender_name))
+    return render_template('lab9/greeting.html', form=form)
+
+@lab9.route('/lab9/show_greeting/<recipient_name>/<recipient_gender>/<sender_name>')
+def show_greeting(recipient_name, recipient_gender, sender_name):
+    return render_template('lab9/show_greeting.html', recipient_name=recipient_name, recipient_gender=recipient_gender, sender_name=sender_name)
